fe510764ab4452cf961bbe5ee4dc51e9fc8bf316
Shivani Agarwal <shivaniagar7@gmail.com>
2025-04-07 18:56:10 +0530

    fixed testRoutes error

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express= require('express');
 const mongoose= require('mongoose');
 const cors= require('cors');
-const testRoutes= require('./routes/testRoutes');
+// const testRoutes= require('./routes/testRoutes');
 require('dotenv').config();
 
 const app= express();
