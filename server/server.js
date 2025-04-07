db45cbcea0e319d5904793f9fc77db085a744212
Shivani Agarwal <shivaniagar7@gmail.com>
2025-04-07 18:46:31 +0530

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express= require('express');
+const mongoose= require('mongoose');
+const cors= require('cors');
+const testRoutes= require('./routes/testRoutes');
+require('dotenv').config();
+
+const app= express();
+
+app.use(cors());
+
+app.use(express.json());
+
+// app.use('/api/tests', testRoutes);
+
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch(err => console.log(err));
